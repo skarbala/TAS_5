d0944b036a8a19bbb8565671bee002bce3fdc151
natal <natalia.vajdova@kiwi.com>
2024-01-29 19:15:52 +0100

    3.lekcia

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://furbo.sk/",
+    baseUrl: "https://furbo.sk/waw/",
     retries: {
       openMode: 1,
       runMode: 0,
