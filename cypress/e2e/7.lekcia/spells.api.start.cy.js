870516ad40f1cca5e2a112be6f9ae19451cf323a
martin <skarbala.martin@gmail.com>
2024-02-11 16:29:24 +0100

    updates start of lecture 7 and 8

--- cypress/e2e/7.lekcia/spells.api.start.cy.js
+++ cypress/e2e/7.lekcia/spells.api.start.cy.js
@@ -16,8 +16,5 @@ describe('Spells API', () => {
 
   it('adds new spell', () => {
     // add one specific spell
-    // HOMEWORK 0: check response after spell is created, extract ID and send a request to it, check that spell data are correct
-    // HOMEWORK 1: open page, find spell by effect, open detail and check spell name
-    // HOMEWORK 2: extract spell from request body to standalone JSON constant
   })
 })
--- cypress/e2e/8.lekcia/spells.api.start.cy.js
+++ cypress/e2e/8.lekcia/spells.api.start.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="Cypress" />
+
 const backendBaseUrl = 'http://localhost:3000'
 //Potter API documentation https://documenter.getpostman.com/view/6199862/SzYewFs9#1a29699c-be80-40f2-a355-9ad03cc34d77
 it('Usage of JWT token: get all houses', () => {
+    //TODO
+    //1. zavolat login endpoint
+    //2. vytiahnut token
+    //3. pouzit token vo volani houses endpointu
     cy.request(backendBaseUrl + '/houses').then(response => {
         expect(response.body).to.have.length(4)
         expect(response.body[0].name).to.eq('Gryffindor')
