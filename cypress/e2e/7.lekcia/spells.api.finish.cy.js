f78a7917f2ecb7592421b4825979daaabbf5263a
martin <skarbala.martin@gmail.com>
2024-02-12 19:20:40 +0100

    adds finish of the lecture

--- cypress/e2e/7.lekcia/spells.api.finish.cy.js
+++ cypress/e2e/7.lekcia/spells.api.finish.cy.js
@@ -1,49 +1,42 @@
 /// <reference types="Cypress" />
-const backendBaseUrl = 'http://localhost:3000'
-const frontendBaseUrl = 'http://localhost:8080'
-beforeEach('reset spells', () => {
-  cy.request(backendBaseUrl + '/spells/actions/reset')
-})
+// PotterAPI documentation https://documenter.getpostman.com/view/6199862/SzYewFs9
 
 describe('Spells API', () => {
-  it('returns list of spells', () => {
-    cy.request(backendBaseUrl + '/spells')
-      .then(response => {
-        const spells = response.body
-        //over ze sa mi vratilo pole 
-        expect(spells)
-          .to.be.an('array')
-          .and.have.length.greaterThan(0)
 
-        spells.forEach(spell => {
-          expect(spell.effect)
-            .to.be.a('string')
-        });
-      })
+  it('returns list of spells', () => {
+    cy.request('http://localhost:3000/spells').then(response => {
+      cy.log(response.body)
+      expect(response.body).to.have.length(151)
+      response.body.forEach(element => {
+        expect(element.effect).not.to.be.empty
+      });
+    })
   })
 
   it('returns a specific spell', () => {
-    cy.request(backendBaseUrl + '/spells/5b74efe73228320021ab6251')
-      .then(response => {
-        expect(response.body.spell).to.equal('Deprimo')
-      })
+    cy.request('http://localhost:3000/spells/5b74ede13228320021ab6236').then(response => {
+      expect(response.body.spell).not.to.be.empty
+      expect(response.body.effect).to.equal('shoots water from wand')
+    })
   })
-  it('adds new spell', () => {
+
+  it.only('adds new spell', () => {
+    cy.request('http://localhost:3000/spells/actions/reset')
     const newSpell = {
-      spell: 'Corona',
-      type: 'Curse',
-      effect: 'sneezing forever',
+      spell: "Soplik",
+      effect: "sneezing forever",
+      type: "Curse",
       isUnforgivable: true
     }
-    cy.request(
-      {
-        method: 'POST',
-        url: backendBaseUrl + '/spells',
-        body: newSpell
-      }).then(response => {
-        expect(response.body.message).to.eq('Spell created')
-        cy.request(backendBaseUrl + '/spells/' + response.body.spell.id)
-          .then(response => expect(response.body.effect).to.eq(newSpell.effect))
-      })
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3000/spells',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: newSpell
+    })
+    cy.visit('http://localhost:8080/#/spelleology')
+    cy.contains(newSpell.effect).click()
   })
 })
